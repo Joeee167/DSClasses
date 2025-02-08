1f6a8758c6ff99fb40e43efb10684f4ca625c062
Joeee167 <youssefgamer037@gmail.com>
2025-02-08 12:48:54 +0200

    Added MyQueue Class

--- DLLClass/DLLClass.cpp
+++ DLLClass/DLLClass.cpp
@@ -1,30 +1,62 @@
-#include<iostream>
-#include "clsDblLinkedList.h"
+#include <iostream>
+#include "clsMyQueue.h"
 
 using namespace std;
 
 int main()
 {
 
-	clsDblLinkedList <int> MydblLinkedList;
+    clsMyQueue <int> MyQueue;
 
-	
-	MydblLinkedList.InsertAtBeginning(5);
-	MydblLinkedList.InsertAtBeginning(4);
-	MydblLinkedList.InsertAtBeginning(3);
-	MydblLinkedList.InsertAtBeginning(2);
-	MydblLinkedList.InsertAtBeginning(1);
-	MydblLinkedList.InsertAtEnd(6);
-
-	cout << "\nLinked List Content : ";
-	MydblLinkedList.PrintList();
-
-	cout << "\nNumber of items in the list : " << MydblLinkedList.Size() << endl;
+    MyQueue.push(10);
+    MyQueue.push(20);
+    MyQueue.push(30);
+    MyQueue.push(40);
+    MyQueue.push(50);
 
 
-	MydblLinkedList.InsertAfter(10, 100);
-	MydblLinkedList.PrintList();
-	
-	return 0;
+    cout << "\nQueue: \n";
+    MyQueue.Print();
+
+    cout << "\nQueue Size: " << MyQueue.Size();
+    cout << "\nQueue Front: " << MyQueue.front();
+    cout << "\nQueue Back: " << MyQueue.back();
+
+    MyQueue.pop();
+
+    cout << "\n\nQueue after pop() : \n";
+    MyQueue.Print();
+
+    cout << "\n\n Item(2) : " << MyQueue.GetItem(2);
+
+    MyQueue.Reverse();
+    cout << "\n\nQueue after reverse() : \n";
+    MyQueue.Print();
+
+    MyQueue.UpdateItem(2, 600);
+    cout << "\n\nQueue after updating Item(2) to 600 : \n";
+    MyQueue.Print();
+
+    MyQueue.InsertAfter(2, 800);
+    cout << "\n\nQueue after Inserting 800 after Item(2) : \n";
+    MyQueue.Print();
+
+
+    MyQueue.InsertAtFront(1000);
+    cout << "\n\nQueue after Inserting 1000 at front: \n";
+    MyQueue.Print();
+
+    MyQueue.InsertAtBack(2000);
+    cout << "\n\nQueue after Inserting 2000 at back: \n";
+    MyQueue.Print();
+
+    MyQueue.Clear();
+    cout << "\n\nQueue after Clear(): \n";
+    MyQueue.Print();
+
+
+
+
+    system("pause>0");
 
 }
